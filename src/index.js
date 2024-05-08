7f753da0cbfb49d9bc6d6d4f3b9e0d5c83491f13
BLSSMA <162845981+BLSSMA@users.noreply.github.com>
2024-05-08 13:20:45 -0400

    added search functionality with API

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
-function handleSearchSubmit(event){
-  event.preventDefault();
-  let searchInput = document.querySelector("#search-entry");
-  let cityElement = document.querySelector("#current-city");
-  cityElement.innerHTML = searchInput.value;
-  searchInput = searchInput.capitilazeFirstLetter;
+function refreshWeather(response){
+  let temperatureElement = document.querySelector(`#current-temperature`);
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector(`#current-city`);
+
+  temperatureElement.innerHTML = Math.round(temperature);
+  cityElement.innerHTML = response.data.city;
 }
 
-let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", handleSearchSubmit)
+function searchCity(city){
+let apiKey = `01dd2bca25c0t00b3d253f443e0of791`;
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+axios.get(apiUrl).then(refreshWeather);
+}
+
+function handleSearchSubmit(event){
+  event.preventDefault();
+  let searchInput = document.querySelector(`#search-entry`);
+  let cityElement = document.querySelector(`#current-city`);
+  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
+}
+
+  let searchFormElement = document.querySelector(`#search-form`);
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Boston");
